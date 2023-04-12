84a42a3639409416f2dcf95e001304b8c64741c6
zverevs <129664415+zverevs@users.noreply.github.com>
2023-04-12 20:20:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 235789175 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    pval = MMD(compute_kernel="rbf", gamma=1).test(x, y)[1]
+    alpha = 0.08 
+    
+    if pval < alpha:
+        return True
+    else:
+        return False
